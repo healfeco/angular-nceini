e8d715dfd561e0097588070c3dc61976d52ef33c
healfeco <hector.felix@arteaga.mx>
2019-10-20 12:23:50 -0600

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, FooterComponent, HeaderComponent, LoginFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
